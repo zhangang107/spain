ef5fa9a9bfe739739abf3005700ae7a38a47dde3
zhangang <915562062@qq.com>
2018-04-18 16:10:40 +0800

    规范语义分析测试

--- test/test_semantic.py
+++ test/test_semantic.py
@@ -5,7 +5,7 @@
 # @Email:  zhanganguc@gmail.com
 # @Filename: test_semantic.py
 # @Last modified by:   zhangang
-# @Last modified time: 2018-04-09T17:07:24+08:00
+# @Last modified time: 2018-04-13T16:51:57+08:00
 # @Copyright: Copyright by USTC
 
 import sys
@@ -24,20 +24,31 @@ bf = BinFile(filenames)
 print bf.diff_filter()
 
 # import ipdb; ipdb.set_trace()
-graph_o, graph_p = bf.next_func_graphs().next()
+# graph_o, graph_p = bf.next_func_graphs().next()
+#
+# nodes_o_list = [[1, 2, 3], [7, 8, 9]]
+# nodes_p_list = [[1, 2, 3], [7, 8, 9]]
+# sem_data = bf.get_seman_data(nodes_o_list, nodes_p_list)
+# print sem_data
+# '''
+# 测试语义分析
+# '''
+# asms_o = None
+# asms_p = None
+# addrs_o = None
+# addrs_o = None
+# sim = Semantic(*sem_data[0], load_args=filenames)
+# print sim.get_pre_state()
+# print sim.get_post_state()
+# print sim.get_semidiff()
 
-nodes_o_list = [[1, 2, 3], [7, 8, 9]]
-nodes_p_list = [[1, 2, 3], [7, 8, 9]]
-sem_data = bf.get_seman_data(nodes_o_list, nodes_p_list)
-print sem_data
-'''
-测试语义分析
-'''
-asms_o = None
-asms_p = None
-addrs_o = None
-addrs_o = None
-sim = Semantic(*sem_data[0], load_args=filenames)
-print sim.get_pre_state()
-print sim.get_post_state()
-print sim.get_semidiff()
+graph_o, graph_p = bf.get_func_graphs('dtls1_process_heartbeat')
+t = Trace(graph_o, graph_p)
+trace = t.get_trace()
+nodes = t.traces2nodes()
+sem_data = bf.get_seman_data(*nodes)
+for _sem in sem_data:
+    sim = Semantic(*_sem, load_args=filenames)
+    print sim.get_pre_state()
+    print sim.get_post_state()
+    print sim.get_semidiff()
