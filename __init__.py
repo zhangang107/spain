1247ec8ecc235f2862817b9c5dbe380e74c2c12b
zhangang <915562062@qq.com>
2018-04-24 09:47:27 +0800

    将spain模块化

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Author: zhangang <zhangang>
+# @Date:   2018-04-24T09:46:03+08:00
+# @Email:  zhanganguc@gmail.com
+# @Filename: __init__.py
+# @Last modified by:   zhangang
+# @Last modified time: 2018-04-24T09:46:17+08:00
+# @Copyright: Copyright by USTC
+
+from setting import *
+from log import comlog
+from session import Session
